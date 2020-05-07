ee2f8374a378ded8d1568b790c738b644a4090bd
Jing Wei Li <ljw9801055@gmail.com>
2020-05-07 01:15:19 -0400

    Completed Home Page Desktop Design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    adcAppDownloadLink = 'https://apps.apple.com/us/app/automatic-door-control/id1500529300';
+    adcWebsite = 'https://rpiadc.com';
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
